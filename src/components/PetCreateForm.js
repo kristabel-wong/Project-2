fa7020fccb233143f09dca6d449706481edaf259
123mandy <xuetingsun1213@gmail.com>
2022-05-01 02:01:47 +1000

    more data to form and little css

--- src/components/PetCreateForm.js
+++ src/components/PetCreateForm.js
@@ -1,8 +1,9 @@
 import {useState, useEffect} from "react";
 import {db,storage} from "../firebase-config";
 import {addDoc, collection, getDocs} from "firebase/firestore";
-import { ref, uploadBytes } from "firebase/storage";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import {v4} from "uuid";
+import "./PetCreateForm.css"
 
 
 function PetCreateForm() {
@@ -14,6 +15,10 @@ function PetCreateForm() {
     const [newAge, setNewAge] = useState(0);
     const [newDOB, setNewDOB] = useState(null);
     const [newUrl, setNewUrl] = useState(null);
+    const [newType, setNewType] = useState("");
+    const [newGender, setNewGender] = useState("");
+    const [newLocation, setNewLocation] = useState("");
+    const [newDescription, setNewDescription] = useState("");
 
 
     const petsCollectionRef = collection(db, "pets");
@@ -22,15 +27,17 @@ function PetCreateForm() {
 
     // you can upload a image right now
     // every time choose a file, run this function
-    let uniqUrl = null;
     const onFileChange = async ( ) =>{
         if(newUrl == null) return;
         const uniqImageName = v4()+newUrl.name;
         const imageRef = ref(storage, `images/${uniqImageName}`)
-        uploadBytes(imageRef, newUrl).then(()=>{
-           alert("upload");
-           console.log(uniqImageName)
-        })
+
+        // you need use then() to handle promises, it NEED time to fetch the info! or use await!
+        await uploadBytes(imageRef, newUrl).then(()=>{
+           alert("File upload");
+        });
+
+        getDownloadURL(imageRef).then((url)=> {setNewUrl(url)})
     }
     
     const createPet = async () => {   
@@ -38,7 +45,10 @@ function PetCreateForm() {
                 name: newName,
                 age: newAge,
                 dob: newDOB,
-                imageUrl : uniqUrl
+                imageUrl : newUrl,
+                gender: newGender,
+                location: newLocation,
+                description: newDescription
             })  
     }
 
@@ -53,17 +63,34 @@ function PetCreateForm() {
     },[])
 
         return(
-            <div>             
-                <label>Name:</label>
-                <input placeholder="Enter the name of your pet..." required onChange={(event)=>{setNewName(event.target.value)}}/>
-                <label>Date of birth:</label>
-                <input type="date" required onChange={(event)=>{setNewDOB(event.target.value)}}/>
-                <label>Age:</label>
-                <input type="number" required onChange={(event)=>{setNewAge(event.target.value)}}/>
-                <label>Photos:</label>
-                <input type="file" onChange={(event) =>{setNewUrl(event.target.files[0])}}/>
-                <input type="submit" onClick={onFileChange} value="Upload" />
-                <input type="submit" value="Submit" onClick={createPet}/>
+            <div className="container">  
+                <h1>Describe your pet</h1>           
+                <label className="form-label" >Name:</label>
+                <input className="form-field" placeholder="Enter the name of your pet..." required onChange={(event)=>{setNewName(event.target.value)}}/>
+
+                <label className="form-label">Date of birth:</label>
+                <input className="form-field" type="date" required onChange={(event)=>{setNewDOB(event.target.value)}}/>
+
+                <label className="form-label">Age:</label>
+                <input className="form-field" type="number" required onChange={(event)=>{setNewAge(event.target.value)}}/>
+
+                <label className="form-label">Type:</label>
+                <input className="form-field" required onChange={(event)=>{setNewType(event.target.value)}}/>
+
+                <label className="form-label">Gender:</label>
+                <input className="form-field" required onChange={(event)=>{setNewGender(event.target.value)}}/>
+
+                <label className="form-label">Location:</label>
+                <input className="form-field" required onChange={(event)=>{setNewLocation(event.target.value)}}/>
+    
+                <label className="form-label">Photos:</label>
+                <input className="form-field" type="file" onChange={(event) =>{setNewUrl(event.target.files[0])}}/>
+                <button className="upload-button" onClick={onFileChange} value="Upload" > Upload Image </button>
+
+                <label className="form-label">Description:</label>
+                <textarea required rows="5" cols="60" onChange={(event)=>{setNewDescription(event.target.value)}}/>
+
+                <button className="upload-button" onClick={createPet}> Submit Form </button>
 
                 {pets.map((pet)=> {
 
