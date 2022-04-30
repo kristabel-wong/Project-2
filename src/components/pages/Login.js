d9614d350e487b0e738574dd7315d4d0808750fd
Dhayananth Santhamoorthy <dhaya@192-168-1-117.tpgi.com.au>
2022-04-30 18:31:33 +1000

    basic authentication

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,7 +1,70 @@
-import React from "react";
+import { async } from "@firebase/util";
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { auth, signInWithGoogle } from "../../firebase-config";
 
 function Login() {
-	return <div>Login</div>;
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [user, setUser] = useState({});
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const logout = async () => {
+    await auth.signOut();
+    setUser(null);
+  };
+  return (
+    <div>
+      <div>
+        <h3>Login</h3>
+        <input
+          placeholder="Email..."
+          name="loginEmail"
+          value={loginEmail}
+          onChange={(event) => {
+            setLoginEmail(event.target.value);
+          }}
+        />
+        <input
+          placeholder="Password..."
+          name="loginPassword"
+          value={loginPassword}
+          onChange={(event) => {
+            setLoginPassword(event.target.value);
+          }}
+        />
+        <button onClick={login}> Login </button>
+      </div>
+
+      <h4> User Logged In:</h4>
+      <h4>{user?.email}</h4>
+
+      <div>
+        <button onClick={signInWithGoogle}> Sign in with google</button>
+      </div>
+      <NavLink to={"/"}>
+        <button onClick={logout}>Sign Out</button>
+      </NavLink>
+    </div>
+  );
 }
 
 export default Login;
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "@firebase/firestore";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,16 +9,35 @@ import { getFirestore } from "@firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: "project-2-5825e.firebaseapp.com",
-	projectId: "project-2-5825e",
-	storageBucket: "project-2-5825e.appspot.com",
-	messagingSenderId: "1036084019168",
-	appId: "1:1036084019168:web:a99cc60d4a4816cde4f585",
-	measurementId: "G-9QF6W07Z6W",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "project-2-5825e.firebaseapp.com",
+  projectId: "project-2-5825e",
+  storageBucket: "project-2-5825e.appspot.com",
+  messagingSenderId: "1036084019168",
+  appId: "1:1036084019168:web:a99cc60d4a4816cde4f585",
+  measurementId: "G-9QF6W07Z6W",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const db = getFirestore(app);
+// export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const name = result.user.displayName;
+      const email = result.user.email;
+      const profilePic = result.user.photoURL;
+
+      localStorage.setItem("name", name);
+      localStorage.setItem("email", email);
+      localStorage.setItem("profilePic", profilePic);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
