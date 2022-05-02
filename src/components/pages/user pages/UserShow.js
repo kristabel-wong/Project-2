32820d0bdd158538e62664b03a942b0d3cea4179
Dhayananth Santhamoorthy <dhaya@192-168-1-117.tpgi.com.au>
2022-05-02 14:18:08 +1000

    user profile version-1

--- src/components/pages/user pages/UserShow.js
+++ src/components/pages/user pages/UserShow.js
@@ -1,24 +1,36 @@
 import { link, useParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
-import { db } from "../../../firebase-config";
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { db, auth } from "../../../firebase-config";
+import { doc, getDoc } from "firebase/firestore";
 
 // this is just for example for setting up routes dont use this
 function User() {
-  //   let params = useParams();
-  //   const [userInfo, setUserInfo] = useState([]);
+  let params = useParams();
+  const [userInfo, setUserInfo] = useState(null);
+  // const uid = params.id;
+  let data;
 
-  //   async function getUser(uid) {
-  //     // getting data from db here
+  const getUser = async (uid) => {
+    const userDocRef = doc(db, "users", uid);
+    const userDocSnap = await getDoc(userDocRef);
+    data = await userDocSnap._document.data.value.mapValue.fields;
 
-  //     setUserInfo("data");
-  //   }
+    setUserInfo(data);
+  };
 
-  //   useEffect(() => {
-  //     getUser(params.id);
-  //   }, [params.id, userInfo]);
+  useEffect(() => {
+    if (userInfo === null) {
+      getUser(params.id);
+    }
+  }, []);
 
-  return <div>User</div>;
+  return (
+    <div>
+      <div>
+        {userInfo === null ? "" : <div>{userInfo.firstName.stringValue}</div>}
+      </div>
+    </div>
+  );
 }
 
 export default User;
