7e3490d5156c0c0a6a355b033c0f7d41eb15e0a4
kristabel-wong <kristabel.wong@hotmail.com>
2022-05-03 18:22:55 +1000

    css styling for users in message page

--- src/components/User.js
+++ src/components/User.js
@@ -3,16 +3,17 @@ import React from "react";
 // this component is for the chat index page
 function User({ user, selectUser }) {
 	return (
-		<div className="user-wrapper" onClick={() => selectUser(user)}>
-			<div className="user-info">
-				<img style={{width: '40px'}}
+		<div className="user-wrapper" onClick={() => selectUser(user)} >
+            <h1 style={{margin: '10px', textAlign: 'center'}} >Users</h1>
+			<div className="user-info" style={{display: 'grid', gridTemplateColumns: '60px 150px'}}>
+				<img style={{width: '50px'}} className="userIcon"
 					src={
 						// user.img ||
 						"https://cdn-icons-png.flaticon.com/512/141/141783.png"
 					}
 				/>
-				<h4>{user.firstName}</h4>
-				<div></div>
+				
+				<div style={{lineHeight: '50px'}}><h4>{user.firstName}</h4></div>
 			</div>
 		</div>
 	);
